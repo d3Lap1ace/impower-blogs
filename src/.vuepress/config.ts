9470fb952f9157d0901b101700e77fed8d95ce3b
d3lap1ace L.Lewis <ap1cem@163.com>
2024-07-14 17:01:06 +0800

    dp3

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/impower-blogs/",
 
   lang: "zh-CN",
   title: "impower",
@@ -15,15 +15,3 @@ export default defineUserConfig({
   // shouldPrefetch: false,
 });
 
-
-module.exports = {
-    title: '我的博客',
-    description: '全栈知识库',
-    theme: 'rope',
-    base: '/impower-blogs/',
-    locales: {
-        '/': {
-          lang: 'zh-CN'
-        }
-      },
-}
