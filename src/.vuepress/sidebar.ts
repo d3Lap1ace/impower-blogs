975ddef7f74d2c254f9598af601d9120c37df830
Lucas Lewis <83862243+d3Lap1ace@users.noreply.github.com>
2024-09-05 00:28:36 +0800

    java

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,9 +1,11 @@
 import {SidebarConfig4Multiple} from "vuepress/config";
 import JavaSideBar from "./sidebars/JavaSideBar.js";
 import DbSideBar from "./sidebars/DbSideBar.js";
+import MiddlewareSideBar from "./sidebars/MiddlewareSideBar.js";
 
 export default{
     "/Java/": JavaSideBar,
     "/DataBase/": DbSideBar,
+    "/Middleware/": MiddlewareSideBar,
 
 } as SidebarConfig4Multiple;
